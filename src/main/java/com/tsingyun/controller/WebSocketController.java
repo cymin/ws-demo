fd1ac63646764018d08799b94ac2b5561b6300f4
cymin <cymin2016@163.com>
2017-05-05 13:00:04 +0800

    服务器主动推送消息

--- src/main/java/com/tsingyun/controller/WebSocketController.java
+++ src/main/java/com/tsingyun/controller/WebSocketController.java
@@ -6,16 +6,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
 
 @Controller
+@EnableScheduling
 public class WebSocketController {
 
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
+
+    @Scheduled(fixedDelay = 1000L)
+    public void send() throws InterruptedException {
+        Thread.sleep(1000);
+        String s = "message: " + Math.random() * 1000;
+        messagingTemplate.convertAndSend("/topic/", s);
+        System.out.println(s);
+    }
+
+    @RequestMapping("/timely")
+    public String timely(){
+        return "timely";
+    }
 
     @MessageMapping("/chat")
     public void handleChat(Principal principal, Message message) {
